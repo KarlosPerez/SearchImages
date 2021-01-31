a62c7e530073885489ffbc562787ceb8559655cb
Carlos Andres <legomarth@hotmail.com>
2021-01-31 18:36:36 -0500

    feat: implement search functionality

--- app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,14 @@
 package com.karlosprojects.searchimagesapp.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.karlosprojects.searchimagesapp.R
 import com.karlosprojects.searchimagesapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,6 +34,9 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
                 header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
                 footer = UnsplashPhotoLoadStateAdapter { adapter.retry() }
             )
+            galleryBtnRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         /** its important to pass viewLifecycleOwner to the livedata method and not the fragment itself
@@ -36,6 +44,51 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                galleryProgressbar.isVisible = loadState.source.refresh is LoadState.Loading
+                galleryRecycler.isVisible = loadState.source.refresh is LoadState.NotLoading
+                galleryBtnRetry.isVisible = loadState.source.refresh is LoadState.Error
+                galleryTxtViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+                //Empty view
+                if(loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    adapter.itemCount < 1) {
+                    galleryRecycler.isVisible = false
+                    galleryTxtViewEmpty.isVisible = true
+                } else {
+                    galleryTxtViewEmpty.isVisible = false
+                }
+            }
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.menu_action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if(query != null) {
+                    binding.galleryRecycler.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 
     override fun onDestroyView() {
