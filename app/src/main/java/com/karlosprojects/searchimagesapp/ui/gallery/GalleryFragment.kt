95cf2158f7bbc6ac568b977eaf3cd25dde8560d2
Carlos Andres <legomarth@hotmail.com>
2021-01-30 23:13:23 -0500

    feat: implement the PagingSource and Pager classes

--- app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryFragment.kt
@@ -1,11 +1,24 @@
 package com.karlosprojects.searchimagesapp.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.karlosprojects.searchimagesapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
+    private val viewModel by viewModels<GalleryViewModel>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /** its important to pass viewLifecycleOwner to the livedata method and not the fragment itself
+        because we want to stop updating our UI when the view of the fragment is destroyed (ex. when fragment is putting to the backstack) **/
+        viewModel.photos.observe(viewLifecycleOwner) {
+
+        }
+    }
 }
--- app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryViewModel.kt
@@ -1,11 +1,34 @@
 package com.karlosprojects.searchimagesapp.ui.gallery
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.karlosprojects.searchimagesapp.data.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
     private val repository: UnsplashRepository
 ) : ViewModel() {
 
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    /**
+     * with swithMap this will be executed whenever the value of the livedata changes; when it happens
+     * we get passed a parameter that contains the new value of currentQuery in form of String
+     *
+     * After we get the result, we cache the livedata, otherwise we get a crash when configuration changes
+     */
+    val photos = currentQuery.switchMap { queryString ->
+        repository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query : String) {
+        currentQuery.value = query
+    }
+
+    companion object {
+        private const val DEFAULT_QUERY = "cats"
+    }
 }
