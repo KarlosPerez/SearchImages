430000053bc7b23f6c7f0c878f4b0feb4d45eeaf
Carlos Andres <legomarth@hotmail.com>
2021-01-30 23:53:05 -0500

    feat: implement PhotoAdapter and set list of photos

--- app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.karlosprojects.searchimagesapp.R
+import com.karlosprojects.searchimagesapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,13 +13,30 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var _binding : FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentGalleryBinding.bind(view)
+
+        val adapter = UnsplashPhotoAdapter()
+
+        binding.apply {
+            galleryRecycler.setHasFixedSize(true)
+            galleryRecycler.adapter = adapter
+        }
 
         /** its important to pass viewLifecycleOwner to the livedata method and not the fragment itself
         because we want to stop updating our UI when the view of the fragment is destroyed (ex. when fragment is putting to the backstack) **/
         viewModel.photos.observe(viewLifecycleOwner) {
-
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/karlosprojects/searchimagesapp/ui/gallery/GalleryViewModel.kt
@@ -29,6 +29,6 @@ class GalleryViewModel @ViewModelInject constructor(
     }
 
     companion object {
-        private const val DEFAULT_QUERY = "cats"
+        private const val DEFAULT_QUERY = "dogs"
     }
 }
