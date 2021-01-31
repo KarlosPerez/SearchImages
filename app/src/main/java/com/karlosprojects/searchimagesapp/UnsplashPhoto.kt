e1022b045a9a75aa103a81fd088e67960bdddcea
Carlos Andres <legomarth@hotmail.com>
2021-01-30 21:46:22 -0500

    feat: create gallery layout and create photo model

---
+++ app/src/main/java/com/karlosprojects/searchimagesapp/UnsplashPhoto.kt
@@ -0,0 +1,30 @@
+package com.karlosprojects.searchimagesapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val thumb: String,
+        val small: String
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=SearchImagesApp&utm_medium=referral"
+    }
+}
