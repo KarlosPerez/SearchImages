1b18a9124e37bb51b1774aecb83c2f232042da4b
Carlos Andres <legomarth@hotmail.com>
2021-01-31 19:31:28 -0500

    feat: implement DetailFragment photo and back arrow

--- app/src/main/java/com/karlosprojects/searchimagesapp/ui/details/DetailFragment.kt
+++ app/src/main/java/com/karlosprojects/searchimagesapp/ui/details/DetailFragment.kt
@@ -1,9 +1,78 @@
 package com.karlosprojects.searchimagesapp.ui.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.karlosprojects.searchimagesapp.R
+import com.karlosprojects.searchimagesapp.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_details) {
+
+    private val args by navArgs<DetailFragmentArgs>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val photo = args.photo
+
+            /*We pass this@DetailFragment and not the view, because the view is less efficient and
+            has some problems. So we send the fragment because it's the contained class */
+            Glide.with(this@DetailFragment)
+                .load(photo.urls.regular)
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        detailsProgressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        detailsProgressBar.isVisible = false
+                        detailsTxtCreator.isVisible = true
+                        detailsTxtDescription.isVisible = photo.description != null
+                        return false
+                    }
+                })
+                .into(detailsImg)
+
+            detailsTxtDescription.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            detailsTxtCreator.apply {
+                text = "Photo by ${photo.user.name} on Unsplash"
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+    }
 }
